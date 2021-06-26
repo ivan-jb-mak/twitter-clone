d655e08d2ef4a05ccc833215b36a33371b157b82
ivanjbmak <ivanjbmak@gmail.com>
2021-06-25 23:41:38 -0400

    adding node-sass to project, and editing landing page apperance  with scss

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,34 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import twitterLogo from "../styles/assets/twitter-logo.png";
 import twitterLogo from "../styles/assets/twitter-logo-white.png";
-import "../styles/landing.css";
+// import "../styles/landing.css";
+import "../scss/pages/Landing.scss";
 
 const Landing = () => {
   return (
     <div className="main">
-      <div className="wrapper">
-        <div className="left"></div>
-
-        <div className="center">
-          <Link to="/">
-            <img
-              src={twitterLogo}
-              alt="twitter-logo"
-              style={{ width: "50px" }}
-              className="logo"
-            />
+      <div className="left"></div>
+      <div className="right">
+        <Link to="/">
+          <img
+            src={twitterLogo}
+            alt="twitter-logo"
+            style={{ width: "50px" }}
+            className="logo"
+          />
+        </Link>
+        <h1 className="main-heading">Happening now</h1>
+        <span className="sub-heading">Join Twitter today.</span>
+        <div className="buttons">
+          <Link to="/signup" className="btn-sign-up">
+            Sign up
           </Link>
-          <h1 className="main-heading">Happening now</h1>
-          <span className="sub-heading">Join Twitter today.</span>
-          <div className="buttons">
-            <Link to="/signup" className="btn-sign-up">
-              Sign up
-            </Link>
-            <Link to="/login" className="btn-login">
-              Log in
-            </Link>
-          </div>
+          <Link to="/login" className="btn-login">
+            Log in
+          </Link>
         </div>
       </div>
     </div>
