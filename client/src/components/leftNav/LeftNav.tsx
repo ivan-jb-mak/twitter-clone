de7cbb2925fecacbd14cbf52637044f13bd6ef33
ivan-jb-mak <ivanjbmak@gmail.com>
2021-06-29 12:52:50 -0400

    editing button mixin in scss

--- client/src/components/leftNav/LeftNav.tsx
+++ client/src/components/leftNav/LeftNav.tsx
@@ -13,52 +13,52 @@ const LeftNav = () => {
         <img src={favicon} alt="logo" style={{ width: "30px" }} />
       </Link>
       <Link to="/home" className="home-button">
-        <h2>
+        <div>
           <i className="fa fa-home" aria-hidden="true" />{" "}
           <span className="title">Home</span>
-        </h2>
+        </div>
       </Link>
       <Link to="/home" className="explore-button">
-        <h2>
+        <div>
           <i className="fa fa-home" aria-hidden="true" />{" "}
           <span className="title">Explore</span>
-        </h2>
+        </div>
       </Link>
       <Link to="/users" className="notifications-button">
-        <h2>
+        <div>
           <i className="fa fa-bell" aria-hidden="true" />{" "}
           <span className="title">Notifications</span>
-        </h2>
+        </div>
       </Link>
       <Link to="/users" className="messages-button">
-        <h2>
+        <div>
           <i className="fa fa-envelope" aria-hidden="true" />{" "}
           <span className="title">Messages</span>
-        </h2>
+        </div>
       </Link>
       <Link to="/home" className="bookmarks-button">
-        <h2>
+        <div>
           <i className="fa fa-home" aria-hidden="true" />{" "}
           <span className="title">Bookmarks</span>
-        </h2>
+        </div>
       </Link>
       <Link to="/home" className="lists-button">
-        <h2>
+        <div>
           <i className="fa fa-home" aria-hidden="true" />{" "}
           <span className="title">Lists</span>
-        </h2>
+        </div>
       </Link>
       <Link to="/profile" className="profile-button">
-        <h2>
+        <div>
           <i className="fa fa-user" aria-hidden="true" />{" "}
           <span className="title">Profile</span>
-        </h2>
+        </div>
       </Link>
       <Link to="/users" className="more-button">
-        <h2>
+        <div>
           <i className="fa fa-ellipsis-h" aria-hidden="true" />{" "}
           <span className="title">More</span>
-        </h2>
+        </div>
       </Link>
       <Tweet />
       <Logout />
